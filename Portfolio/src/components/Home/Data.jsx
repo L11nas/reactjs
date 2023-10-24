82930c4038e0b3d49819931f5752226e908554fd
L11nas <ulevicius.linas@gmail.com>
2023-10-25 00:54:30 +0300

    some correction

--- Portfolio/src/components/Home/Data.jsx
+++ Portfolio/src/components/Home/Data.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import svgImage from '.src/assets/Linas.svg';
 const Data = () => {
   return (
     <div className='home__data'>
       <h1 className='home__title'>Linas Ulevičius</h1>
+      <img src={svgImage} alt='SVG Image' />
     </div>
   );
 };
