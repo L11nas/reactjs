dc8420e63f989413f3e224f42914675d5a7409c1
L11nas <ulevicius.linas@gmail.com>
2023-11-03 09:02:10 +0200

    skills

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/Home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/Sklills';
 import Services from './components/services/Services';
+import Qualification from './components/qualification/Qualification';
 const App = () => {
   return (
     <>
@@ -14,6 +15,7 @@ const App = () => {
         <About />
         <Skills />
         <Services />
+        <Qualification />
       </main>
     </>
   );
--- Portfolio/src/components/services/Services.jsx
+++ Portfolio/src/components/services/Services.jsx
@@ -1,40 +1,165 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './services.css';
 
 const Services = () => {
+  const [activeTab, setActiveTab] = useState(null);
+
+  const openModal = (index) => {
+    setActiveTab(index);
+  };
+
+  const closeModal = () => {
+    setActiveTab(null);
+  };
+
   return (
     <section className='services section' id='services'>
       <h2 className='section__title'>Services</h2>
-      <span className='section__subtitle'>What i offer</span>
-      <div className='services__container grid'>
+      <span className='section__subtitle'>What I offer</span>
+
+      <div className='services__container container grid'>
+        {/* Service 1 */}
         <div className='services__content'>
           <div>
-            <i className='uil uil services_icon'></i>
-            <h3 className='srvices__title'></h3>
+            <i className='uil uil-web-grid services__icon'></i>
+            <h3 className='services__title'>
+              Product
+              <br />
+              Designer
+            </h3>
           </div>
 
-          <span className='services__button'>
+          <span className='services__button' onClick={() => openModal(1)}>
             View More{' '}
             <i className='uil uil-arrow-right services__button-icon'></i>
           </span>
-          <div className='services__modal'></div>
-          <div className='services__modal-content'>
-            <i className='uil-times services__modal-close'></i>
-            <h3 className='services__modal-title'></h3>
-            <p className='services__modal-description'>Darbo patirtis</p>
 
-            <ul className='services__modal-services-grid'>
-              <li className='services__modal-service'>
-                <i className='uil uil-check-circle services__modal-icon'>
-                  <p className='services__modal-info'>
-                    sukuriau at ir ta vartototjo sasaja
-                  </p>
-                </i>
-              </li>
-            </ul>
+          {activeTab === 1 && (
+            <div className='services__modal active__modal'>
+              <div className='services__modal-content'>
+                <i
+                  onClick={closeModal}
+                  className='uil-times services__modal-close'
+                ></i>
+                <h3 className='services__modal-title'>
+                  Product Designer Details
+                </h3>
+                <p className='services__modal-description'>work experience</p>
+                <ul className='services__modal-services-grid'>
+                  <li className='services__modal-service'>
+                    <i className='uil uil-check-circle services__modal-icon'></i>
+                    <p className='services__modal-info'>what I create</p>
+                  </li>
+                  <li className='services__modal-service'>
+                    <i className='uil uil-check-circle services__modal-icon'></i>
+                    <p className='services__modal-info'>
+                      I position your company brand
+                    </p>
+                  </li>
+                  <li className='services__modal-service'>
+                    <i className='uil uil-check-circle services__modal-icon'></i>
+                    <p className='services__modal-info'>
+                      Design and mockups of products for companies
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          )}
+        </div>
+
+        {/* Service 2 */}
+        <div className='services__content'>
+          <div>
+            <i className='uil uil-arrow services__icon'></i>
+            <h3 className='services__title'>Ui/Ux Designer</h3>
           </div>
+
+          <span className='services__button' onClick={() => openModal(2)}>
+            View More{' '}
+            <i className='uil uil-arrow-right services__button-icon'></i>
+          </span>
+
+          {activeTab === 2 && (
+            <div className='services__modal active__modal'>
+              <div className='services__modal-content'>
+                <i
+                  onClick={closeModal}
+                  className='uil-times services__modal-close'
+                ></i>
+                <h3 className='services__modal-title'>
+                  Ui/Ux Designer Details
+                </h3>
+                <p className='services__modal-description'>work experience</p>
+                <ul className='services__modal-services-grid'>
+                  <li className='services__modal-service'>
+                    <i className='uil uil-check-circle services__modal-icon'></i>
+                    <p className='services__modal-info'>what I create</p>
+                  </li>
+                  <li className='services__modal-service'>
+                    <i className='uil uil-check-circle services__modal-icon'></i>
+                    <p className='services__modal-info'>
+                      I position your company brand
+                    </p>
+                  </li>
+                  <li className='services__modal-service'>
+                    <i className='uil uil-check-circle services__modal-icon'></i>
+                    <p className='services__modal-info'>
+                      Design and mockups of products for companies
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          )}
+        </div>
+
+        {/* Service 3 */}
+        <div className='services__content'>
+          <div>
+            <i className='bx bx-edit services__icon'></i>
+            <h3 className='services__title'>Other Service</h3>
+          </div>
+
+          <span className='services__button' onClick={() => openModal(3)}>
+            View More{' '}
+            <i className='uil uil-arrow-right services__button-icon'></i>
+          </span>
+
+          {activeTab === 3 && (
+            <div className='services__modal active__modal'>
+              <div className='services__modal-content'>
+                <i
+                  onClick={closeModal}
+                  className='uil-times services__modal-close'
+                ></i>
+                <h3 className='services__modal-title'>Other Service Details</h3>
+                <p className='services__modal-description'>work experience</p>
+                <ul className='services__modal-services-grid'>
+                  <li className='services__modal-service'>
+                    <i className='uil uil-check-circle services__modal-icon'></i>
+                    <p className='services__modal-info'>what I create</p>
+                  </li>
+                  <li className='services__modal-service'>
+                    <i className='uil uil-check-circle services__modal-icon'></i>
+                    <p className='services__modal-info'>
+                      I position your company brand
+                    </p>
+                  </li>
+                  <li className='services__modal-service'>
+                    <i className='uil uil-check-circle services__modal-icon'></i>
+                    <p className='services__modal-info'>
+                      Design and mockups of products for companies
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </section>
   );
 };
+
 export default Services;
